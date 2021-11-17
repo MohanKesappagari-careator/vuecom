26dcd93ba6eef7aecabec05bfcf3429bfa48d6d5
MohanKesappagari-careator <mohan.k@careator.com>
2021-11-17 14:21:20 +0530

    header

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <div class="navbar navbar-dark bg-primary shadow-sm">
+      <div class="navabr-brad align-item-center d-flex">
+        <i class="fas fa-shipping-fast logo">&nbsp;<strong>Kart</strong></i>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style>
+.logo {
+  color: white;
+  font-size: 3rem;
+  margin-left: 0.8rem;
+}
+strong {
+  font-size: 2rem;
+}
+</style>
